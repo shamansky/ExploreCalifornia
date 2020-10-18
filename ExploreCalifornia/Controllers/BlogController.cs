ecb31a0fe3d6bfcb683f294bf31150d085f0ac4f
George Ciurescu <shubst3r@gmail.com>
2020-10-18 23:43:47 +0100

    minor controller updates

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -14,16 +14,13 @@ namespace ExploreCalifornia.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return new ContentResult { Content = "blog posts" };
+            return View();
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-                return new ContentResult {
-                    Content = string.Format("Year: {0}; Month: {1}; Key: {2}",
-                    year, month, key)
-            };
+            return View();
         }
     }
 }
