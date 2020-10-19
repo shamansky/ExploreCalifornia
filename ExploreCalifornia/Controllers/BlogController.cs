dafbb4c81ee08daca04837302d458458f627683e
George Ciurescu <shubst3r@gmail.com>
2020-10-19 20:19:48 +0100

    changes in the folder structure

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,14 +12,46 @@ namespace ExploreCalifornia.Controllers
     [Route("blog")]
     public class BlogController : Controller
     {
-        // GET: /<controller>/
+        [Route("")]
         public IActionResult Index()
         {
-            return View();
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "George",
+                    Body = "This is a great blog post, don't you think?",
+                },
+                new Post
+                {
+                    Title = "My second blog post",
+                    Posted = DateTime.Now,
+                    Author = "George",
+                    Body = "This is ANOTHER great blog post, don't you think?",
+                },
+            };
+
+            return View(posts);
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
+        {
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "George",
+                Body = "This is a great blog post, don't you think?",
+            };
+
+            return View(post);
+        }
+
+        [Route("create")]
+        public IActionResult Create()
         {
             return View();
         }
